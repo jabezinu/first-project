20de9549cfe8d441b2bf96fd085d61bb4113dadf
jabezinu <jabezzinu31@gmail.com>
2025-07-10 04:23:11 +0300

    edit controller ,route and model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -19,7 +21,26 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(),[
+            'title' => 'required|string|min:3|max:255',
+            'body' => 'required|string|min:5',
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => false,
+                'message' => 'All fields are required',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+            $post = Post::create($request->all());
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Post created successfully',
+                'data' => $post
+            ], 201);
     }
 
     /**
